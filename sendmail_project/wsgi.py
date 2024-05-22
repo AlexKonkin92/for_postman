a970a10fbcedded4f3263f42c555351971eca33e
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-22 16:40:50 +0300

    api

--- sendmail_project/wsgi.py
+++ sendmail_project/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+from ipalib import api
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sendmail_project.settings')
+
+if not api.isdone('bootstrap'):
+    api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works')
+    api.finalize()
+    api.Backend.rpcclient.connect()
 
 application = get_wsgi_application()
