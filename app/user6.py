8d57c8b5ba2b2ece05364f9b10035de2a1f93755
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-22 14:12:02 +0300

    add api

--- app/user6.py
+++ app/user6.py
@@ -4,10 +4,8 @@ import smtplib
 from email.mime.text import MIMEText
 from ipalib import api
 
-api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works')
 api.finalize()
 api.Backend.rpcclient.connect()
-
 
 def generate_password():
     return "new_password"
