a02ede2217623945524ef6257896d1049e90af5b
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-22 14:00:59 +0300

    changes for test

--- app/user6.py
+++ app/user6.py
@@ -2,10 +2,20 @@
 
 import smtplib
 from email.mime.text import MIMEText
+from ipalib import api
 
 
 def generate_password():
     return "new_password"
+
+def valid_user(email):
+    try:
+        result = api.Command.user_find(mail=email)['result']
+        if result:
+            user_username = result[0]['uid'][0]
+            return user_username,email
+    except Exception as e:
+        return False, str(e)
     
 def send_email(recipient, password):
     sender = 'ya.alexgr4@yandex.ru'
@@ -22,12 +32,10 @@ def send_email(recipient, password):
         
 def reset_password(email):
     #email = "ya.alexgr4@yandex.ru"
-    #user_username, user_email = valid_user(email)
+    user_username, user_email = valid_user(email)
     new_password = generate_password()
-    #print(f"Сгенерированный пароль: {new_password}")
-    send_email(email, new_password)
-    #print("Пароль отправлен на почту.")
-
+    send_email(user_email, new_password)
+    api.Command.user_mod(user_username, userpassword=new_password)
     try:
         return True, "Пароль успешно изменен и отправлен на электронную почту."
     except Exception as e:
