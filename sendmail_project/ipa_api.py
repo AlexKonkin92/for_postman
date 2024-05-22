30f17897389262a8a50b609d166fe309ee107a5c
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-22 18:33:25 +0300

    before func into ipa_lib

--- sendmail_project/ipa_api.py
+++ sendmail_project/ipa_api.py
@@ -1,6 +1,7 @@
 import logging 
 
 def bootstrap_ipa_api():
+    logging.warning(f'in func before: {IPA_API._API__done}')
     from ipalib import api as IPA_API
     logging.warning(f'in func: {IPA_API._API__done}')
     #IPA_API.bootstrap()
