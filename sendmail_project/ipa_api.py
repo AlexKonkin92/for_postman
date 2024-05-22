e0eea54abf6567b67286efdd869136bafd03cbd3
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-22 18:52:04 +0300

    add create_api in ipa_api

--- sendmail_project/ipa_api.py
+++ sendmail_project/ipa_api.py
@@ -3,11 +3,14 @@ import os
 import threading
 
 def bootstrap_ipa_api():
-    from ipalib import api as IPA_API
+    from ipalib import create_api
+    IPA_API = create_api(mode=None)
     logging.warning(f'in func: {IPA_API._API__done}')
     logging.warning(os.getpid())
     logging.warning(threading.current_thread())
-    #IPA_API.bootstrap()
-    #IPA_API.finalize()
+    if 'finalize' in IPA_API._API__done:
+        return IPA_API
+    IPA_API.bootstrap()
+    IPA_API.finalize()
     IPA_API.Backend.rpcclient.connect()
     return IPA_API
