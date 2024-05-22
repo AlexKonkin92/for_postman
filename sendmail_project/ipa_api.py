65a60fd60b579ea7bee3e8cff09ebc23a86d7099
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-22 18:37:33 +0300

    add os and threading

--- sendmail_project/ipa_api.py
+++ sendmail_project/ipa_api.py
@@ -1,9 +1,12 @@
 import logging 
+import os
+import threading
 
 def bootstrap_ipa_api():
-    logging.warning(f'in func before: {IPA_API._API__done}')
     from ipalib import api as IPA_API
     logging.warning(f'in func: {IPA_API._API__done}')
+    logging.warning(os.getpid())
+    logging.warning(threading.current_thread())
     #IPA_API.bootstrap()
     #IPA_API.finalize()
     IPA_API.Backend.rpcclient.connect()
