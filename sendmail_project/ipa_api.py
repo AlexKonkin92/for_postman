303215d54c5927e1da1c4b75b473e0a2f0883d3e
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-22 22:56:42 +0300

    add Lock

--- sendmail_project/ipa_api.py
+++ sendmail_project/ipa_api.py
@@ -1,16 +1,19 @@
-import logging 
-import os
+import logging
 import threading
+from ipalib import api as IPA_API
+
+initialization_lock = threading.Lock()
 
 def bootstrap_ipa_api():
-    from ipalib import create_api
-    IPA_API = create_api(mode=None)
-    logging.warning(f'in func: {IPA_API._API__done}')
-    logging.warning(os.getpid())
-    logging.warning(threading.current_thread())
-    if 'finalize' in IPA_API._API__done:
-        return IPA_API
-    IPA_API.bootstrap()
-    IPA_API.finalize()
-    IPA_API.Backend.rpcclient.connect()
+    with initialization_lock:
+        logging.warning(f'in func: {IPA_API._API__done}')
+        logging.warning(os.getpid())
+        logging.warning(threading.current_thread())
+        if 'finalize' in IPA_API._API__done:
+            logging.warning('IPA API is already bootstrapped.')
+            return IPA_API
+        logging.warning('Bootstrapping IPA API.')
+        IPA_API.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works')
+        IPA_API.finalize()
+        IPA_API.Backend.rpcclient.connect()
     return IPA_API
