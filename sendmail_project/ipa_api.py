0f725e6810441303306455e6ad32b3f492f91cac
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-23 12:27:34 +0300

    add coments in ipa_api "if"

--- sendmail_project/ipa_api.py
+++ sendmail_project/ipa_api.py
@@ -8,9 +8,9 @@ initialization_lock = threading.Lock()
 def bootstrap_ipa_api():
     with initialization_lock:
         logging.warning(threading.current_thread())
-        if 'finalize' in IPA_API._API__done:
-            logging.warning('IPA API is already bootstrapped.')
-            return IPA_API
+        # if 'finalize' in IPA_API._API__done:
+        #     logging.warning('IPA API is already bootstrapped.')
+        #     return IPA_API
         logging.warning('Bootstrapping IPA API.')
         IPA_API = create_api(mode=None)
         IPA_API.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works')
