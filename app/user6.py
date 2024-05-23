c456a176b80b89601c905f482d4e30cb2c110ede
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-23 11:28:07 +0300

    try to block django-main-thread

--- app/user6.py
+++ app/user6.py
@@ -10,21 +10,38 @@ from ipalib import api #
 logging.basicConfig(level=logging.WARNING)
 logging.warning(f"before initialization_lock: {threading.current_thread()}")
 initialization_lock = threading.Lock() #
-logging.warning(f"after initialization_lock: {threading.current_thread()}")
+initialization_lock.acquire()
+logging.warning(f"after initialization_lock and acquire: {threading.current_thread()}")
 
-with initialization_lock:
+try:
     if 'finalize' in api._API__done:
         logging.warning('IPA API is already bootstrapped.')
     else:
         logging.warning(threading.current_thread())
         logging.warning('Bootstrapping IPA API.')
-        api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works' , in_server=True)
+        api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works', in_server=True)
         api.finalize()
         if api.env.in_server:
             api.Backend.ldap2.connect()
         else:
             api.Backend.rpcclient.connect()
-logging.warning(f"after with initialization_lock: {threading.current_thread()}")
+finally:
+    # Освобождаем блокировку после инициализации
+    initialization_lock.release()
+
+# with initialization_lock:
+#     if 'finalize' in api._API__done:
+#         logging.warning('IPA API is already bootstrapped.')
+#     else:
+#         logging.warning(threading.current_thread())
+#         logging.warning('Bootstrapping IPA API.')
+#         api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works' , in_server=True)
+#         api.finalize()
+#         if api.env.in_server:
+#             api.Backend.ldap2.connect()
+#         else:
+#             api.Backend.rpcclient.connect()
+logging.warning(f"after with try: {threading.current_thread()}")
 #IPA_API = bootstrap_ipa_api()
 #logging.warning(f'without func: {IPA_API._API__done}')
 
@@ -57,69 +74,3 @@ def reset_password():
     except Exception as e:
         logging.warning('error sending password.')
         return False, str(e)
-
-# import smtplib
-# from email.mime.text import MIMEText
-# import logging
-# import threading
-# from ipalib import api
-
-
-# logging.basicConfig(level=logging.WARNING)
-
-# class IPABootstrapThread(threading.Thread):
-#     def __init__(self):
-#         super().__init__()
-#         self._stop_event = threading.Event()
-
-#     def stop(self):
-#         self._stop_event.set()
-
-#     def run(self):
-#         with threading.Lock():
-#             if 'finalize' not in api._API__done and not self._stop_event.is_set():
-#                 logging.warning('Bootstrapping IPA API.')
-#                 api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works', in_server=True)
-#                 api.finalize()
-#                 if api.env.in_server:
-#                     api.Backend.ldap2.connect()
-#                 else:
-#                     api.Backend.rpcclient.connect()
-#             else:
-#                 logging.warning('IPA API is already bootstrapped.')
-
-# ipa_bootstrap_thread = IPABootstrapThread()
-# ipa_bootstrap_thread.start()
-
-
-# # ipa_bootstrap_thread.stop()
-# # ipa_bootstrap_thread.join()
-
-# def generate_password():
-#     return "new_password"
-    
-# def send_email(recipient, password):
-#     sender = 'ya.alexgr4@yandex.ru'
-#     message = f"Временный пароль: {password}" 
-#     msg = MIMEText(message)
-    
-#     msg['Subject'] = 'Временный пароль для FreeIPA'
-#     msg['From'] = sender
-#     msg['To'] = recipient
-
-#     with smtplib.SMTP_SSL('smtp.yandex.ru', 465) as server:
-#         server.login(sender, 'xsegyibpinputkgo')
-#         server.sendmail(sender, recipient, msg.as_string())
-
-# def reset_password():
-#     email = "ya.alexgr4@yandex.ru"
-#     user_username = "alex"
-#     new_password = generate_password()
-#     send_email(email, new_password)
-#     api.Command.user_mod(user_username, userpassword=new_password)
-#     try:
-#         return True, "Пароль успешно изменен и отправлен на электронную почту."
-#     except Exception as e:
-#         return False, str(e)
-
-# ipa_bootstrap_thread.join()
