624f7859b8201d34af7953c62550ff10c8d30d98
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-23 12:14:29 +0300

    add sendmail_project.ipa_api into views and add put into message IPA_API.Command.user_show

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from .user6 import reset_password
 import logging
 import threading 
+from sendmail_project.ipa_api import bootstrap_ipa_api
 
 def reset_password_view(request):
 
@@ -12,12 +13,19 @@ def reset_password_view(request):
         
         #success, message = reset_password()
         success = True
-        message = "test"
+        
         logging.warning("Test")
         logging.warning(threading.current_thread())
-        if success:
+        IPA_API = bootstrap_ipa_api()
+        
+        try:
+            message = IPA_API.Command.user_show('newuser', all=True)['result']
             return JsonResponse({'status': 'success', 'message': message})
-        else:
-            return JsonResponse({'status': 'error', 'message': message}, status=500)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
+        # if success:
+        #     return JsonResponse({'status': 'success', 'message': message})
+        # else:
+        #     return JsonResponse({'status': 'error', 'message': message}, status=500)
     else:
         return JsonResponse({'status': 'error', 'message': 'Invalid request method.'}, status=405)
