bdb0ceb18d8f4a74d45d7b609fdb2eb195b9a5df
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-27 17:54:30 +0300

    check commands

--- try_for_freeipa.py
+++ try_for_freeipa.py
@@ -1,48 +1,28 @@
-from ipalib import create_api
+from ipalib import api
 from ipalib.rpc import jsonclient
 
-# Создание нового экземпляра API
-api = create_api()
+# Создание и инициализация API
+api.bootstrap_with_global_options(context='cli', debug=True, verbose=True)
+api.finalize()
 
-# Логирование инициализации API
-print("Инициализация API")
+# Подключение к RPC клиенту
+client = jsonclient(api)
+client.finalize()
+client.connect()
 
+# Проверка доступных команд
 try:
-    # Инициализация API
-    api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works')
-    print("Bootstrap завершен")
-    
-    # Принудительная регистрация всех плагинов
-    api.load_plugins()
-    print("Плагины загружены")
-    
-    api.finalize()
-    print("Finalize завершен")
+    # Проверяем доступные команды
+    available_commands = list(api.Command)
+    print(f"Доступные команды: {available_commands}")
 
-except Exception as e:
-    print(f"Ошибка инициализации API: {e}")
-
-# Подключение API через RPC клиент
-print("Подключение к RPC клиенту")
-
-try:
-    client = jsonclient(api)
-    client.finalize()
-    print("RPC клиент финализирован")
-    
-    client.connect()
-    print("RPC клиент подключен")
-
-except Exception as e:
-    print(f"Ошибка подключения RPC клиента: {e}")
-
-# Проверка конфигурации окружения и домена
-try:
-    env = api.Command['env']()
-    print(f"Конфигурация окружения: {env}")
-
-    domain = api.Command['whoami']()
-    print(f"Информация о домене: {domain}")
+    # Попытка вызова команды 'user_find' если она существует
+    if 'user_find' in available_commands:
+        email = 'ya.alexgr4@yandex.ru'
+        result = api.Command['user_find'](mail=email)
+        print(f"Результат выполнения команды 'user_find': {result}")
+    else:
+        print("Команда 'user_find' недоступна")
 
 except KeyError as e:
     print(f"Команда не найдена: {e}")
