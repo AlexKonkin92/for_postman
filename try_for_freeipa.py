79ddc988269165808b8e41ca01131abb68b8f793
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-27 17:57:43 +0300

    try to make a command "user_find"

--- try_for_freeipa.py
+++ try_for_freeipa.py
@@ -1,19 +1,51 @@
-from ipalib import api
+from ipalib import create_api
 from ipalib.rpc import jsonclient
 
-# Создание и инициализация API
-api.bootstrap_with_global_options(context='cli', debug=True, verbose=True)
-api.finalize()
+# Создание нового экземпляра API
+api = create_api()
 
-# Подключение к RPC клиенту
-client = jsonclient(api)
-client.finalize()
-client.connect()
+# Логирование инициализации API
+print("Инициализация API")
 
-# Проверка доступных команд
 try:
-    # Проверяем доступные команды
-    available_commands = list(api.Command)
+    # Печатаем конфигурацию API перед bootstrap
+    config = api.env
+    print(f"Конфигурация перед bootstrap: {config}")
+    
+    # Инициализация API
+    api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works')
+    print("Bootstrap завершен")
+
+    # Принудительная регистрация всех плагинов
+    api.load_plugins()
+    print("Плагины загружены")
+
+    api.finalize()
+    print("Finalize завершен")
+
+except Exception as e:
+    print(f"Ошибка инициализации API: {e}")
+
+# Подключение API через RPC клиент
+print("Подключение к RPC клиенту")
+
+try:
+    client = jsonclient(api)
+    client.finalize()
+    print("RPC клиент финализирован")
+    
+    client.connect()
+    print("RPC клиент подключен")
+
+except Exception as e:
+    print(f"Ошибка подключения RPC клиента: {e}")
+
+# Подробное логирование доступных плагинов
+try:
+    plugins = list(api.loaded_plugins)
+    print(f"Загруженные плагины: {plugins}")
+
+    available_commands = list(api.Command.keys())
     print(f"Доступные команды: {available_commands}")
 
     # Попытка вызова команды 'user_find' если она существует
