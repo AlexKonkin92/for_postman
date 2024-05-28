5a70edd75027ce3b53d97bc8cd814a6e7845b30c
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-28 10:43:09 +0300

    add api.Command.user_find in try for freeipa

--- try_for_freeipa.py
+++ try_for_freeipa.py
@@ -1,27 +1,13 @@
 from ipalib import create_api
-from ipalib import errors
-from ipalib.backend import Executioner
-from ipalib.plugable import Registry
 
-# Создание API объекта
+# Создание и инициализация API объекта
 api = create_api(mode=None)
-
-# Инициализация API
 api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works')
-
-# Загрузка плагинов и команд
 api.finalize()
 
 # Подключение к серверу
 api.Backend.rpcclient.connect()
 
-# Явная загрузка команд
-executioner = Executioner(api)
-api.Command = Registry(api, 'command', api.env.context)
-
 # Выполнение команды
-try:
-    result = api.Command['user_find']()
-    print(result)
-except errors.PublicError as e:
-    print(f"An error occurred: {e}")
+result = api.Command.user_find()
+print(result)
