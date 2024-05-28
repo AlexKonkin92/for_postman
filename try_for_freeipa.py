ad524f781b4e5597ff703ddf215ca3872927458a
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-28 10:59:23 +0300

    chaeck  loaded_plugins in try for freeipa

--- try_for_freeipa.py
+++ try_for_freeipa.py
@@ -1,4 +1,5 @@
 from ipalib import create_api, rpc
+from ipalib import errors
 
 # Создание нового экземпляра API
 api = create_api()
@@ -13,14 +14,26 @@ client.finalize()
 client.connect()
 
 # Проверка загруженных плагинов
-print("Loaded plugins:", api._plugins.keys())
+try:
+    print("Loaded plugins:", api.env.loaded_plugins)
+except AttributeError:
+    print("Could not access loaded plugins.")
 
 # Проверка доступных команд
-print("Available commands:", api.Command.keys())
+try:
+    available_commands = api.Command.keys()
+    print("Available commands:", available_commands)
+except AttributeError:
+    print("Could not access available commands.")
 
 # Выполнение команды user_find, если она доступна
-if 'user_find' in api.Command:
-    result = api.Command['user_find']()
-    print(result)
-else:
+try:
+    if 'user_find' in api.Command:
+        result = api.Command['user_find']()
+        print(result)
+    else:
+        print("The command 'user_find' is not available in the current API context.")
+except errors.PublicError as e:
+    print(f"An error occurred: {e}")
+except KeyError:
     print("The command 'user_find' is not available in the current API context.")
