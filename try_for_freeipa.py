4b72bf2524484c6aa1f0052e2e13bb0de9874d30
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-28 11:43:31 +0300

    del api.load_plugins and add the command user_show

--- try_for_freeipa.py
+++ try_for_freeipa.py
@@ -1,9 +1,12 @@
 from ipalib import create_api, rpc
+from ipaclient.plugins.user import user_show
+
+
 api = create_api()
 api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works')
+api.add_plugin(user_show)
 
-
-api.load_plugins
+#api.load_plugins
 
 api.finalize()
 client = rpc.jsonclient(api)
@@ -22,10 +25,12 @@ try:
 except AttributeError:
     print("нет команд.")
 try:
-    if 'user_find' in api.Command:
-        result = api.Command['user_find']()
+    if 'user_show' in api.Command:
+        result = api.Command['user_show']()
+        user_show = api.Command.user_show('alex')['result']
+        print(user_show)
         print(result)
     else:
-        print("'user_find' не доступна.")
+        print("'user_show' не доступна.")
 except Exception as e:
     print(f"An error occurred: {e}")
