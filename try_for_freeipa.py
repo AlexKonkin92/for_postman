011934fcdf1dc51f19fddf2335c353f8e45bfe50
AlexKonkin92 <konkin.alex777@mail.ru>
2024-05-28 13:13:22 +0300

    add class user_show in try for freeipa

--- try_for_freeipa.py
+++ try_for_freeipa.py
@@ -1,12 +1,22 @@
-from ipalib import create_api, rpc
+from ipalib import create_api, output, rpc
 #from ipaclient.plugins.user import user_show
+from ipalib import frontend
 
-
+# Создаём новый экземпляр API
 api = create_api(mode=None)
+
+# Пример: Определение пользовательской команды
+class user_show(frontend.Command):
+    __doc__ = 'Вывод информации о пользователе'
+    takes_args = ('uid', )
+    has_output = output.Output('result', label='Информация о пользователе')
+api.add_plugin(user_show)
+
 api.bootstrap(context='cli', domain='ks.works', server='freeipa-dev.ks.works')
 #api.add_plugin(user_show)
 
 #api.load_plugins
+
 
 api.finalize()
 client = rpc.jsonclient(api)
@@ -15,27 +25,25 @@ client.connect()
 
 try:
     # Пример команды: получение информации о пользователе
-    user_info = api.Command.user_show(uid='admin')
-    print(f"Информация о пользователе: {user_info['result']}")
-
-    # Ваш код для работы с API
+    user_info2 = client.forward('user_show', uid='admin')
+    print(f"Информация о пользователе: {user_info2}")
 
 except Exception as e:
     print(f"Ошибка при выполнении команды: {e}")
 
-try:
-    print("Loaded plugins:", api.env.loaded_plugins)
-except AttributeError:
-    print("нет плагинов")
-try:
-    available_commands = api.Command.keys()
-    print("команды:", available_commands)
-except AttributeError:
-    print("нет команд.")
-if 'user_show' in api.Command:
-    result = api.Command['user_show']()
-    user_show = api.Command.user_show('alex')['result']
-    print(user_show)
-    print(result)
-else:
-    print("'user_show' не доступна.")
+# try:
+#     print("Loaded plugins:", api.env.loaded_plugins)
+# except AttributeError:
+#     print("нет плагинов")
+# try:
+#     available_commands = api.Command.keys()
+#     print("команды:", available_commands)
+# except AttributeError:
+#     print("нет команд.")
+# if 'user_show' in api.Command:
+#     result = api.Command['user_show']()
+#     user_show = api.Command.user_show('alex')['result']
+#     print(user_show)
+#     print(result)
+# else:
+#     print("'user_show' не доступна.")
